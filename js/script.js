fede64c69af8554b88053bfeebeb5a814fa6d796
Leo <leopold.von-der-weid@hetic.net>
2017-09-05 22:16:22 +0200

    Test on sidebar

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ var wowhead_tooltips = {
         "dropchance": true
     }
 };
+
+// Scroll TO
+
+$('a[href^="#"]').click(function(){
+    var the_id = $(this).attr("href");
+
+    $('html, body').animate({
+        scrollTop:$(the_id).offset().top
+    }, 'slow');
+    return false;
+});
+
